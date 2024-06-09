927cb0d8073d9a952d764b94241621579ab2bba1
Sanjay.vengala <Sanjay.vengala@.com>
2024-06-09 11:56:38 +0530

    added heml charts

--- Backend/obj/Debug/net6.0/MongoDBDotNET6.AssemblyInfo.cs
+++ Backend/obj/Debug/net6.0/MongoDBDotNET6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MongoDBDotNET6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cb5aaa5bccad0c79589475902d3d09d6dcaa92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MongoDBDotNET6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MongoDBDotNET6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
